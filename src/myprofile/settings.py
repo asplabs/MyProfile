71bf8607abfad39172fb4e154d6f342113d65f28
Arun Shanker Prasad <arunshanker@qburst.com>
2013-08-21 10:52:49 +0530

    Removed stray print statement

--- src/myprofile/settings.py
+++ src/myprofile/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'myprofile.urls'
 PROJECT_PATH = os.path.dirname(__file__)
-print PROJECT_PATH
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
